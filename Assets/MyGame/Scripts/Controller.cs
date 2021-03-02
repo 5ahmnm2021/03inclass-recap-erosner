600213af7f4876e48c593901195e3d8335cd78ef
elenajmr <elena.rosner@htl-salzburg.ac.at>
2021-03-02 10:38:56 +0100

    added SceneManagement

---
+++ Assets/MyGame/Scripts/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene01()
+    {
+        SceneManager.LoadScene("01ColorScene");
+    }
+
+    public void ChangeScene02()
+    {
+        SceneManager.LoadScene("02NumberScene");
+    }
+
+    public void ChangeSzene00()
+    {
+        SceneManager.LoadScene("00WelcomeScene");
+    }
+
+}
