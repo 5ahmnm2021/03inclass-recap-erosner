44bac4c3e640beb4012b8c9f4dc97ea1195f49ee
elenajmr <elena.rosner@htl-salzburg.ac.at>
2021-03-09 10:32:30 +0100

    02NumberScene functionality

--- Assets/MyGame/Scripts/Controller.cs
+++ Assets/MyGame/Scripts/Controller.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
+   // public int ScenesToLoad;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +20,20 @@ public class Controller : MonoBehaviour
         
     }
 
+    /*
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(ScenesToLoad);
+    }
+
+    public void LoadNextSceneAutomated()
+    {
+        SceneManager.LoadScene(SceneManager.GeActiveScene().buildIndex +1);
+
+    }
+    */
+
+    
     public void ChangeScene01()
     {
         SceneManager.LoadScene("01ColorScene");
@@ -27,9 +44,25 @@ public class Controller : MonoBehaviour
         SceneManager.LoadScene("02NumberScene");
     }
 
-    public void ChangeSzene00()
+
+   public void ChangeSzene00()
     {
         SceneManager.LoadScene("00WelcomeScene");
     }
 
+
+   [SerializeField] private InputField ifA; // [SerializedField] - auch wenn private im Inspector sichtbar
+    public InputField ifB;
+    public float Wert;
+    public Text txt;
+
+    public void AddNumbers()
+    {
+        Wert = float.Parse(ifA.text)+ float.Parse(ifB.text);
+        Debug.Log(Wert);
+        txt.text = Wert.ToString();
+    }
+
+
+   
 }
