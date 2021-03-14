45fd76cd5b4c29cccf33e2caaf52cef6c1ea30f8
elenajmr <elena.rosner@htl-salzburg.ac.at>
2021-03-14 18:37:04 +0100

    added try/catch

--- Assets/MyGame/Scripts/Controller.cs
+++ Assets/MyGame/Scripts/Controller.cs
@@ -51,18 +51,45 @@ public class Controller : MonoBehaviour
     }
 
 
-   [SerializeField] private InputField ifA; // [SerializedField] - auch wenn private im Inspector sichtbar
+    public InputField ifA;
     public InputField ifB;
-    public float Wert;
     public Text txt;
+    public Text message;
+
+    public Color rot = Color.red;
+    public Color weiss = Color.white;
+
+    public float ifAFloat;
+    public float ifBFloat;
 
     public void AddNumbers()
     {
-        Wert = float.Parse(ifA.text)+ float.Parse(ifB.text);
-        Debug.Log(Wert);
-        txt.text = Wert.ToString();
+        try
+        {
+            ifAFloat = float.Parse(ifA.text);
+            ifA.image.color = weiss;
+            message.text = "ADD NUMBERS";
+        }
+        catch (System.Exception)
+        {
+            message.text = "Geben Sie bitte eine gültige Zahl ein!";
+            ifA.image.color = rot;
+        }
+
+        try
+        {
+            ifBFloat = float.Parse(ifB.text);
+            ifB.image.color = weiss;
+        }
+        catch (System.Exception)
+        {
+            message.text = "Geben Sie bitte eine gültige Zahl ein!";
+            ifB.image.color = rot;
+        }
+
+        txt.text = (ifAFloat + ifBFloat).ToString();
+        Debug.Log("Die Addition ergibt: " + txt.text);
     }
 
 
-   
 }
